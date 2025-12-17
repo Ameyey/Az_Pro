fc04f38faf3c5e20845822b11167070eaa2fec1c
ameybahugune <asd>
2025-12-17 17:54:21 +0530

    they a nabar chking

--- src/component/ResponsiveMenu.jsx
+++ src/component/ResponsiveMenu.jsx
@@ -5,6 +5,7 @@ import { Link, NavLink } from 'react-router-dom'
 
 const ResponsiveMenu = ({ openNav, setOpenNav }) => {
     const { user } = useUser()
+    console.log(user)
     return (
         <div className={`${openNav ? "left-0" : "-left-[100%]"} fixed bottom-0 top-0 z-20 flex h-screen w-[75%] flex-col justify-between bg-white px-8 pb-6 pt-16 text-black md:hidden rounded-r-xl shadow-md transition-all`}>
             <div>
